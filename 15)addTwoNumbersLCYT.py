69f0e84ecddeececc1722f0f178dafadcbe67a12
menpj <3994084+menpj@users.noreply.github.com>
2025-02-13 18:02:58 +0530

    addTwoNumbers yotutube solution,

--- 15)addTwoNumbersLCYT.py
+++ 15)addTwoNumbersLCYT.py
@@ -4,8 +4,9 @@
 #         self.val = val
 #         self.next = next
 
-#I DEVISED THIS SOLUTION, OKAY SOLUTION
+#OKAY SOLUTION, betetr than mine, youtube based
 # https://leetcode.com/problems/add-two-numbers/?envType=study-plan-v2&envId=top-interview-150
+# https://www.youtube.com/watch?v=wgFPrzTjm7s
 #2. Add Two Numbers
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -14,39 +15,39 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-
-        curr=None
+        dummy=ListNode()
+        curr=dummy
         carry=0
         while l1 or l2:
             
             v1=l1.val if l1 else 0
             v2=l2.val if l2 else 0    
-            a1=ListNode()
-
-            if curr:
-                curr.next=a1
+            
+            
+            
             add=v1+v2+carry
             if add>=10:
                 carry=1
                 add=add%10
-                a1.val=add
+                
             else:
-                a1.val=add
+                
                 carry=0
-            if curr==None:
-                head=a1
-            curr=a1
-            if l1:
-                l1=l1.next
-            if l2:
-                l2=l2.next
-        
-        if carry==1:
-            a1=ListNode(1)
-            if curr:
-                curr.next=a1
             
 
-        return head       
+            curr.next=ListNode(add)
+            
+            curr=curr.next
+            l1=l1.next if l1 else None
+            
+            l2=l2.next if l2 else None
+                
+        
+        if carry==1:
+            curr.next=ListNode(1)
+            
+            
+
+        return dummy.next     
 
         
