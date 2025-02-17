b33c4365e9edfa0ccd9eaeeb5c06560b6f438de3
menpj <3994084+menpj@users.noreply.github.com>
2025-02-17 12:29:52 +0530

    VALID anagram , my solution NOT ideal solution

---
+++ 19)validAnagramLC2.py
@@ -0,0 +1,38 @@
+#mysoution not ideal
+#https://leetcode.com/problems/valid-anagram/?envType=study-plan-v2&envId=top-interview-150
+#242. Valid Anagram
+class Solution(object):
+    def isAnagram(self, charlist, tcharlist):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        #charlist=s
+        #tcharlist=t
+        #print(tcharlist)
+        popcount=0
+        for i in range(len(tcharlist)):
+            #print(i-popcount)
+            
+            if tcharlist[i-popcount] in charlist:
+                #print(i)
+                
+
+                charlist=charlist.replace(tcharlist[i-popcount],"",1)
+                tcharlist=tcharlist[:i-popcount]+tcharlist[i-popcount+1:]
+                popcount+=1
+                #print(magazine)
+            else:
+                #print("executed1")
+                return False
+                  
+                #print(magazine)
+        if charlist or tcharlist: 
+            #print("executed2")
+            #print(charlist)
+            #print(tcharlist)
+            return  False
+        else:
+            return True
+        
