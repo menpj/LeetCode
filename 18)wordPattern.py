eca9aecde660b9f0bb7309ed9429efd4d700d27c
menpj <3994084+menpj@users.noreply.github.com>
2025-02-17 10:56:22 +0530

    word pattern , my solution ideal solution

---
+++ 18)wordPattern.py
@@ -0,0 +1,45 @@
+#290. Word Pattern
+#https://leetcode.com/problems/word-pattern/description/?envType=study-plan-v2&envId=top-interview-150
+#my solution, ideal solution
+class Solution(object):
+    
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        s=s.split()
+        t=pattern
+        len1=len(s)
+        len2=len(t)
+        if not len1==len2:
+            return False
+        map ={}
+        for i in range(len2):
+            if s[i] in map:
+                temp=map[s[i]]
+                if t[i]==temp:
+                    continue
+                else:
+                    return False
+            elif t[i] in map.values():
+                #print("executed")
+                return False
+            elif t[i] not in map.values():
+                map[s[i]]=t[i]
+                
+            
+        return True
+        
+        
+def main():
+    solution= Solution()
+    s=input("Enter string pattern:")
+    t=input("Enter string s:")
+    bool=solution.wordPattern(s,t)
+    if bool:
+        print("Follows pattern.")
+    else:
+        print("Do not follow pattern.")
+main()
