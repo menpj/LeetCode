c811fe089f946bb4a2a00235776838dff0406a70
menpj <3994084+menpj@users.noreply.github.com>
2025-04-24 17:21:30 +0530

    my solution,bad solution

---
+++ 24)1TwoSumLC2.py
@@ -0,0 +1,28 @@
+#1. Two Sum
+#https://leetcode.com/problems/two-sum/
+#My Solution, okay solution , bets so far
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        size=len(nums)
+        numsDict={ item:index for index, item in enumerate(nums) }
+        #print(numsDict)
+        i=0
+        while i<size:
+            
+             
+            
+            value=target-nums[i]
+            #print(value)
+            try:
+                if numsDict[value]!=i:
+                    return [i,numsDict[value]]
+            except KeyError:
+                pass
+                
+            i+=1
