ebf3ae63242d6cd7901a12675cd2659e2dafd84c
menpj <3994084+menpj@users.noreply.github.com>
2025-05-08 18:35:31 +0530

    my solution,not optimal solution best so far, don't know how this works

---
+++ 31)435Non-overlappingIntervalsLC2.py
@@ -0,0 +1,38 @@
+#435. Non-overlapping Intervals
+#https://leetcode.com/problems/non-overlapping-intervals/
+#my solution, not optimal, don't exactly know how this works 
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        intervals.sort(key= lambda i:i[0])
+        #print("Sorted: ")
+        #print(intervals)
+        interval=[]
+        count=0
+        for start,end in intervals:
+            #interval[-1][1]
+            if interval and interval[-1][1]>start:
+                count+=1
+                lastInterval=interval[-1][1]-max(interval[-1][0],start)#works on both min and max
+                curInterval=end-max(interval[-1][0],start) #here too
+                
+                '''if lastInterval>curInterval:
+                    interval[-1]=[start,end]'''
+                if lastInterval>curInterval:
+                    interval[-1]=[start,end]
+                    #print(start)
+                    #print(end)
+                    #print("hey here")
+                    #print(interval[-1])
+                #interval[-1][1]=max(end,interval[-1][1])
+            #elif lastVal<start:
+            else:
+                newlist=[start,end]
+                interval.append(newlist) 
+        #print("Output Interval:")     
+        print(interval)
+        return count
+        
