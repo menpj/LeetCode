929b5c1953bf5c238efb95232de9411f2adbb03a
menpj <3994084+menpj@users.noreply.github.com>
2025-05-09 08:58:46 +0530

    YT SOLUTION,best solution

--- 31)435Non-overlappingIntervalsLCYT.py
+++ 31)435Non-overlappingIntervalsLCYT.py
@@ -16,17 +16,17 @@ class Solution(object):
         count=0
         for start,end in intervals:
             #interval[-1][1]
-            if interval and interval[-1][1]>start:
+            if interval and interval[1]>start:
                 count+=1
-                lastInterval=interval[-1][1]-min(interval[-1][0],start)
-                curInterval=end-min(interval[-1][0],start)
+                #lastInterval=interval[-1][1]-min(interval[-1][0],start)
+                #curInterval=end-min(interval[-1][0],start)
                 
                 '''if lastInterval>curInterval:
                     interval[-1]=[start,end]'''
                 '''if lastInterval>curInterval:
                     interval[-1]=[start,end]'''
-                if end<interval[-1][1]:
-                    interval[-1]=[start,end]
+                if end<interval[1]:
+                    interval=[start,end]
                     #print(start)
                     #print(end)
                     #print("hey here")
@@ -35,7 +35,7 @@ class Solution(object):
             #elif lastVal<start:
             else:
                 newlist=[start,end]
-                interval.append(newlist) 
+                interval=newlist 
         #print("Output Interval:")     
         #print(interval)
         return count
