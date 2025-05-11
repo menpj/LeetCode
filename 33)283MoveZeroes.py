e82259e8d9190e8146dc02323fc036e5934a5ca9
menpj <3994084+menpj@users.noreply.github.com>
2025-05-11 17:39:21 +0530

    my SOLUTION,okay SOLUTION

---
+++ 33)283MoveZeroes.py
@@ -0,0 +1,28 @@
+#283. Move Zeroes
+#https://leetcode.com/problems/move-zeroes/
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        zeroP=0
+        nonZP=0
+        size=len(nums)
+        while zeroP<size and nonZP<size:
+            while zeroP<size and nums[zeroP]!=0:
+                zeroP+=1
+            while nonZP<size and nums[nonZP]==0:
+                nonZP+=1
+            if zeroP<size and nonZP<size:
+                if nonZP>zeroP:
+                    nums[zeroP]=nums[nonZP]
+                    nums[nonZP]=0
+                    nonZP+=1
+                    zeroP+=1
+                else:
+                    nonZP+=1
+            else:
+                break
+        #print(nums)
+        return nums
