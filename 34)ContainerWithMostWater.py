f276a90d0de0ad28681b96a116b1afeb78783f5c
menpj <3994084+menpj@users.noreply.github.com>
2025-05-14 14:00:01 +0530

    my SOLUTION, okay solution

---
+++ 34)ContainerWithMostWater.py
@@ -0,0 +1,21 @@
+#11. Container With Most Water
+#my solution, okay solution, best so far
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        size=len(height)
+        l=0
+        r=size-1
+        curMax=0
+        while l<r:
+            cur=(r-l)*min(height[l],height[r])
+            curMax=max(cur,curMax)
+            
+            if height[l]>height[r]:
+                r-=1
+            else:
+                l+=1
+        return curMax
