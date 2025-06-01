8638316729c51e185722a83fb542f583e705e20c
menpj <3994084+menpj@users.noreply.github.com>
2025-06-01 16:15:44 +0530

    MY solution, OKAY SOLUTION, but best so far

---
+++ 36)1679MaxNumberofK-SumPairsLC.py
@@ -0,0 +1,39 @@
+#https://leetcode.com/problems/max-number-of-k-sum-pairs/
+#MY SOLUTION, OKAY SOLUTION, NOT IDEAL
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        size=len(nums)
+        left=0
+        right=size-1
+        hashMap={}
+        nums.sort()
+        count=0
+        while left<right:
+            val=nums[left]+nums[right]
+            '''print("value")
+            print(val)
+            print(hashMap)
+            print("left")
+            print(left)
+            print("right")
+            print(right)
+            print(" ")'''
+            if k>val:
+                left+=1
+            elif k<val:
+                right-=1
+            else:
+                if left not in hashMap and right not in hashMap:
+                    count+=1
+                    hashMap[left]=nums[left]
+                    hashMap[right]=nums[right]
+                    left+=1
+                    right-=1
+        return count
+    
+    
