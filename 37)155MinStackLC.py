ca8ec20edf92da5013aac137e765f06cd4e88ad7
menpj <3994084+menpj@users.noreply.github.com>
2025-06-03 18:49:21 +0530

    my solution, chatgpt inspired solution when stuck

---
+++ 37)155MinStackLC.py
@@ -0,0 +1,63 @@
+#my solution, chatgpt inspired solution when stuck
+class MinStack(object):
+    
+    
+    def __init__(self):
+        self.stack=[]
+        self.minStack=[]
+        
+        
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        
+        
+
+        if not self.minStack or self.minStack[-1]>=val:
+            
+            self.minStack.append(val)
+            
+        self.stack.append(val)
+        
+       
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        val=self.stack[-1]
+        if val==self.minStack[-1]:
+            #print("deleting last element in minstack")
+            del self.minStack[-1]
+        del self.stack[-1]
+        
+        
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        
+        return self.stack[-1]
+        
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        
+        return self.minStack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
