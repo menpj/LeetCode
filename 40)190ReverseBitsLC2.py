73262f9f698e22dfd4e7ce52682f1e72ad7d7b14
menpj <3994084+menpj@users.noreply.github.com>
2025-06-25 16:51:12 +0530

    yt inspired solution

--- 40)190ReverseBitsLC2.py
+++ 40)190ReverseBitsLC2.py
@@ -1,12 +1,15 @@
+#YT VIDEO INSPITRED SOLUTION,CHECK COMMENT WITH PYTHON CODE
+#https://www.youtube.com/watch?v=UcoN6UjAI64
+
 class Solution:
     # @param n, an integer
     # @return an integer
     def reverseBits(self, n):
         res=0
         for i in range(32):
-            bit=(n>>i)&1
+            bit=n&1
             res=(res<<1)|bit
-    
+            n=n>>1
             
         
         return res
