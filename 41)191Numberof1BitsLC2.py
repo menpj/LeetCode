3d4caeb7b6c58b4b3276d050cea0cd76886dce44
menpj <3994084+menpj@users.noreply.github.com>
2025-07-18 13:38:10 +0530

    yt based solution

--- 41)191Numberof1BitsLC2.py
+++ 41)191Numberof1BitsLC2.py
@@ -1,17 +1,19 @@
-#my solution , good solution
+#https://www.youtube.com/watch?v=5Km3utixwZs
+#yt based solution , good solution
 #https://leetcode.com/problems/number-of-1-bits/
-
 class Solution(object):
     def hammingWeight(self, n):
         """
         :type n: int
         :rtype: int
         """
-        n=bin(n)[2:]
+        
         count=0
-        for i in n:
-            if i=="1":
-                count+=1
+        while n:
+            count+=n%2
+            n=n>>1
+            #print(n)
+
         #print(count)
         return count
         #n=int(n,2)
