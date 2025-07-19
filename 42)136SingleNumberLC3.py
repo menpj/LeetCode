abba20bb021a6df27ccf3bd2ebb39a0a9d912c42
menpj <3994084+menpj@users.noreply.github.com>
2025-07-19 17:46:47 +0530

    youtube inspired solution,not ideal solution, use extra space

---
+++ 42)136SingleNumberLC3.py
@@ -0,0 +1,20 @@
+
+#136. Single Number
+#https://leetcode.com/problems/single-number/
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        array=set()
+        for i in nums:
+            
+            if i in array:
+                array.remove(i)
+            else:
+                array.add(i)
+        #print(array)
+        for i in array:
+            return i
+        
